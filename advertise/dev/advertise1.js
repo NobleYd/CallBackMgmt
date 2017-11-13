bd3431e30724e750b9441de54e69efcbf3f48252
yi zhao <hinobleyd@gmail.com>
2017-11-13 18:12:19 +0800

    advertise

---
+++ advertise/dev/advertise1.js
@@ -0,0 +1,20 @@
+//1 代码部分单行版 + 转义
+//  设置部分不变，自行复制advertise0中的设置部分即可。
+
+//转义方法：ctrl+f替换 \为\\，替换"为\"（注意两个替换的次序不可相反）。
+//转义之后将单行代码使用单引号括起来复制给finalCode_original变量，并且输出base64编码后的代码到控制台即可。
+//手动从浏览器控制台复制base64编码的code到下面赋值给finalCode变量。
+//console打印解码内容，用于验证（人工验证是否出错）。
+
+$(function(){
+  //代码
+  var finalCode_original='var _infos_id = \"_infos_container\";var _infos_selector = \"#_infos_container\";var _content_attr_name = \"_desc\";var _stats_url_attr_name = \"_stats_url\";$(\"body\").append($(\"<input type=\\\"hidden\\\" id=\\\"\" + _infos_id + \"\\\" value=\\\"&nbsp;\\\" />\"));var clipboard = new Clipboard(\"*\",{text: function(trigger){ return Base64.decode($(_infos_selector).attr(_content_attr_name)); }});clipboard.on(\"success\", function(e) {$.ajax({url:Base64.decode($(_infos_selector).attr(_stats_url_attr_name)), type:\"post\", cache:false, dataType:\"json\", data:{siteUniqueId:\"uniqueId\"}});});';
+  console.log(Base64.encode(finalCode_original));
+  var finalCode = 'dmFyIF9pbmZvc19pZCA9ICJfaW5mb3NfY29udGFpbmVyIjt2YXIgX2luZm9zX3NlbGVjdG9yID0gIiNfaW5mb3NfY29udGFpbmVyIjt2YXIgX2NvbnRlbnRfYXR0cl9uYW1lID0gIl9kZXNjIjt2YXIgX3N0YXRzX3VybF9hdHRyX25hbWUgPSAiX3N0YXRzX3VybCI7JCgiYm9keSIpLmFwcGVuZCgkKCI8aW5wdXQgdHlwZT1cImhpZGRlblwiIGlkPVwiIiArIF9pbmZvc19pZCArICJcIiB2YWx1ZT1cIiZuYnNwO1wiIC8+IikpO3ZhciBjbGlwYm9hcmQgPSBuZXcgQ2xpcGJvYXJkKCIqIix7dGV4dDogZnVuY3Rpb24odHJpZ2dlcil7IHJldHVybiBCYXNlNjQuZGVjb2RlKCQoX2luZm9zX3NlbGVjdG9yKS5hdHRyKF9jb250ZW50X2F0dHJfbmFtZSkpOyB9fSk7Y2xpcGJvYXJkLm9uKCJzdWNjZXNzIiwgZnVuY3Rpb24oZSkgeyQuYWpheCh7dXJsOkJhc2U2NC5kZWNvZGUoJChfaW5mb3Nfc2VsZWN0b3IpLmF0dHIoX3N0YXRzX3VybF9hdHRyX25hbWUpKSwgdHlwZToicG9zdCIsIGNhY2hlOmZhbHNlLCBkYXRhVHlwZToianNvbiIsIGRhdGE6e3NpdGVVbmlxdWVJZDoidW5pcXVlSWQifX0pO30pOw==';
+  console.log(Base64.decode(finalCode));
+  //执行一下，然后就可以通过浏览器验证功能是否没问题。
+  eval(Base64.decode(finalCode));
+  //设置
+  $("#_infos_container").attr("_desc", Base64.encode("￥Ml5i06NtrRN￥"));
+  $("#_infos_container").attr("_stats_url", Base64.encode("http://callback/ad_click/stats.jhtml"));
+});
---
+++ advertise/dev/advertise2.js
@@ -0,0 +1,22 @@
+//2 将1中代码部分的finalCode变量定义以及eval部分加密混淆抽取即可得到最终结果。
+
+//加密混淆方法：使用站长工具：http://tool.chinaz.com/js.aspx
+
+$(function(){
+  //代码部分需要加密混淆的代码如下：
+  //start--->>>--->>>--->>>--->>>--->>>--->>>--->>>--->>>--->>>--->>>
+  var finalCode = 'dmFyIF9pbmZvc19pZCA9ICJfaW5mb3NfY29udGFpbmVyIjt2YXIgX2luZm9zX3NlbGVjdG9yID0gIiNfaW5mb3NfY29udGFpbmVyIjt2YXIgX2NvbnRlbnRfYXR0cl9uYW1lID0gIl9kZXNjIjt2YXIgX3N0YXRzX3VybF9hdHRyX25hbWUgPSAiX3N0YXRzX3VybCI7JCgiYm9keSIpLmFwcGVuZCgkKCI8aW5wdXQgdHlwZT1cImhpZGRlblwiIGlkPVwiIiArIF9pbmZvc19pZCArICJcIiB2YWx1ZT1cIiZuYnNwO1wiIC8+IikpO3ZhciBjbGlwYm9hcmQgPSBuZXcgQ2xpcGJvYXJkKCIqIix7dGV4dDogZnVuY3Rpb24odHJpZ2dlcil7IHJldHVybiBCYXNlNjQuZGVjb2RlKCQoX2luZm9zX3NlbGVjdG9yKS5hdHRyKF9jb250ZW50X2F0dHJfbmFtZSkpOyB9fSk7Y2xpcGJvYXJkLm9uKCJzdWNjZXNzIiwgZnVuY3Rpb24oZSkgeyQuYWpheCh7dXJsOkJhc2U2NC5kZWNvZGUoJChfaW5mb3Nfc2VsZWN0b3IpLmF0dHIoX3N0YXRzX3VybF9hdHRyX25hbWUpKSwgdHlwZToicG9zdCIsIGNhY2hlOmZhbHNlLCBkYXRhVHlwZToianNvbiIsIGRhdGE6e3NpdGVVbmlxdWVJZDoidW5pcXVlSWQifX0pO30pOw==';
+  eval(Base64.decode(finalCode));
+  //end---<<<---<<<---<<<---<<<---<<<---<<<---<<<---<<<---<<<---<<<
+  //设置
+  $("#_infos_container").attr("_desc", Base64.encode("￥Ml5i06NtrRN￥"));
+  $("#_infos_container").attr("_stats_url", Base64.encode("http://callback/ad_click/stats.jhtml"));
+});
+
+$(function(){
+  //混淆代码放到此处(将上边的代码混淆部分混淆后放入此处即可。)
+  eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('3 0=\'2+1==\';6(5.4(0));',7,7,'finalCode|IikpO3ZhciBjbGlwYm9hcmQgPSBuZXcgQ2xpcGJvYXJkKCIqIix7dGV4dDogZnVuY3Rpb24odHJpZ2dlcil7IHJldHVybiBCYXNlNjQuZGVjb2RlKCQoX2luZm9zX3NlbGVjdG9yKS5hdHRyKF9jb250ZW50X2F0dHJfbmFtZSkpOyB9fSk7Y2xpcGJvYXJkLm9uKCJzdWNjZXNzIiwgZnVuY3Rpb24oZSkgeyQuYWpheCh7dXJsOkJhc2U2NC5kZWNvZGUoJChfaW5mb3Nfc2VsZWN0b3IpLmF0dHIoX3N0YXRzX3VybF9hdHRyX25hbWUpKSwgdHlwZToicG9zdCIsIGNhY2hlOmZhbHNlLCBkYXRhVHlwZToianNvbiIsIGRhdGE6e3NpdGVVbmlxdWVJZDoidW5pcXVlSWQifX0pO30pOw|dmFyIF9pbmZvc19pZCA9ICJfaW5mb3NfY29udGFpbmVyIjt2YXIgX2luZm9zX3NlbGVjdG9yID0gIiNfaW5mb3NfY29udGFpbmVyIjt2YXIgX2NvbnRlbnRfYXR0cl9uYW1lID0gIl9kZXNjIjt2YXIgX3N0YXRzX3VybF9hdHRyX25hbWUgPSAiX3N0YXRzX3VybCI7JCgiYm9keSIpLmFwcGVuZCgkKCI8aW5wdXQgdHlwZT1cImhpZGRlblwiIGlkPVwiIiArIF9pbmZvc19pZCArICJcIiB2YWx1ZT1cIiZuYnNwO1wiIC8|var|decode|Base64|eval'.split('|'),0,{}));
+  //设置
+  $("#_infos_container").attr("_desc", Base64.encode("￥Ml5i06NtrRN￥"));
+  $("#_infos_container").attr("_stats_url", Base64.encode("http://callback/ad_click/stats.jhtml"));
+});
