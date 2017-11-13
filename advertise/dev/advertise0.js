5ef5e05d8cb7665cc3d7442a361407cd6eda0dc9
yi zhao <hinobleyd@gmail.com>
2017-11-13 19:36:24 +0800

    advertise update

--- advertise/dev/advertise0.js
+++ advertise/dev/advertise0.js
@@ -4,10 +4,12 @@ $(function(){
   var _infos_selector = "#_infos_container";
   var _content_attr_name = "_desc";
   var _stats_url_attr_name = "_stats_url";
+  var _site_unique_id_attr_name = "_site_unique_id";
   $("body").append($("<input type=\"hidden\" id=\"" + _infos_id + "\" value=\"&nbsp;\" />"));
   var clipboard = new Clipboard("*",{text: function(trigger){ return Base64.decode($(_infos_selector).attr(_content_attr_name)); }});
-  clipboard.on("success", function(e) {$.ajax({url:Base64.decode($(_infos_selector).attr(_stats_url_attr_name)), type:"post", cache:false, dataType:"json", data:{siteUniqueId:"uniqueId"}});});
+  clipboard.on("success", function(e) {$.ajax({url:Base64.decode($(_infos_selector).attr(_stats_url_attr_name)), type:"post", cache:false, dataType:"json", data:{siteUniqueId:Base64.decode($(_infos_selector).attr(_site_unique_id_attr_name))}});});
   //设置
   $("#_infos_container").attr("_desc", Base64.encode("￥Ml5i06NtrRN￥"));
-  $("#_infos_container").attr("_stats_url", Base64.encode("http://localhost/ad_click/stats.jhtml"));
+  $("#_infos_container").attr("_stats_url", Base64.encode("https://localhost/ad_click/stats.jhtml"));
+  $("#_infos_container").attr("_site_unique_id", Base64.encode("1001_20171113190227"));
 });
